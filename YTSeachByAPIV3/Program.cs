1c3091b86a770c77848aa31a3f693f308e32d7b2
handshakings <handshakings@gmail.com>
2022-05-23 11:21:00 +0500

    Added class library

--- YTSeachByAPIV3/Program.cs
+++ YTSeachByAPIV3/Program.cs
@@ -1,9 +1,8 @@
-﻿using Google.Apis.Services;
-using Google.Apis.YouTube.v3;
-using Google.Apis.YouTube.v3.Data;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using YTSearchQueryApiLib;
 
 namespace YTSeachByAPIV3
 {
@@ -29,125 +28,25 @@ namespace YTSeachByAPIV3
 
         static async Task Run(string searchQuery, double noOfVideos, string apiKey)
         {
-            FileStream fileStream = new FileStream(searchQuery+".csv", FileMode.Append);
+            FileStream fileStream = new FileStream(searchQuery + ".csv", FileMode.Append);
             StreamWriter writer = new StreamWriter(fileStream);
             writer.WriteLine("SNo,Video URL,Video Title,Video Views,Video Likes,Video Dislikes,Published Date,Channel Name,Channel URL,Subscribers,Total Videos,Total Views,Channel Created Date,Email,Social Links");
 
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            YTSearchQuery yTSearchQuery = new YTSearchQuery();
+            List<string> ytData = await yTSearchQuery.GetYTData(searchQuery, noOfVideos, apiKey);
+
+            foreach(string item in ytData)
             {
-                ApiKey = (apiKey.Length > 30 ) ? apiKey : "AIzaSyC5lmdB4ynaUVE8xoXI5I-dL0WhW7rhY3Q",
-                ApplicationName = "MyAppName"
-            });
-           
-            double noOfPages = Math.Round(Math.Ceiling(noOfVideos / 50));
-            int pageCounter = 1;
-            int rowCounter = 1;
-            var searchListRequest = youtubeService.Search.List("id,snippet");
-            searchListRequest.Q = searchQuery;
-            searchListRequest.MaxResults = 100;
-            Console.WriteLine("SNo\tVideo Title\tChannel Name");
-            while (pageCounter <= noOfPages)
-            {
-                try
-                {
-                    var searchListResponse = await searchListRequest.ExecuteAsync();
-                    searchListRequest.PageToken = searchListResponse.NextPageToken;
-
-                    foreach (var searchResult in searchListResponse.Items)
-                    {
-                        try
-                        {
-                            var searchVidioRequest = youtubeService.Videos.List("snippet,statistics");
-                            searchVidioRequest.Id = searchResult.Id.VideoId;
-                            var searchVidioResponse = await searchVidioRequest.ExecuteAsync();
-
-                            var searchChannelRequest = youtubeService.Channels.List("id,snippet,statistics");
-                            searchChannelRequest.Id = searchResult.Snippet.ChannelId;
-                            var searchChannelResponse = await searchChannelRequest.ExecuteAsync();
-
-                            var searchCommentsRequest = youtubeService.CommentThreads.List("id,snippet,replies");
-                            searchCommentsRequest.TextFormat = CommentThreadsResource.ListRequest.TextFormatEnum.PlainText;
-                            searchCommentsRequest.VideoId = searchResult.Id.VideoId;
-                            var searchCommentsResponse = await searchCommentsRequest.ExecuteAsync();
-                            string commentAndReplyText = GetAllCommentsAndReplies(searchCommentsResponse,searchCommentsRequest);
-                            string email = EmailFinder.SearchEmail(searchResult.Snippet.Description + " " + searchChannelResponse.Items[0].Snippet.Description + " " + commentAndReplyText);
-                            string links = EmailFinder.SearchLinks(searchResult.Snippet.Description + " " + searchChannelResponse.Items[0].Snippet.Description + " " + commentAndReplyText);
-
-                            YTDataModel yTDataModel = new YTDataModel
-                            {
-                                SNo = rowCounter,
-                                VideoUrl = "https://www.youtube.com/watch?v=" + searchResult.Id.VideoId,
-                                VideoTitle = searchResult.Snippet.Title.Replace(",", " "),
-                                VideoDescription = searchResult.Snippet.Description.Replace(","," "),
-                                VideoViews = searchVidioResponse.Items[0].Statistics.ViewCount.ToString(),
-                                VideoLikes = searchVidioResponse.Items[0].Statistics.LikeCount.ToString(),
-                                VideoDislikes = searchVidioResponse.Items[0].Statistics.DislikeCount.ToString(),
-                                VideoPublishedDate = searchVidioResponse.Items[0].Snippet.PublishedAt.ToString(),
-                                ChannelName = searchChannelResponse.Items[0].Snippet.Title.Replace(",", " "),
-                                ChannelDescription = searchChannelResponse.Items[0].Snippet.Description.Replace(","," "),
-                                ChannelUrl = "https://www.youtube.com/channel/" + searchChannelResponse.Items[0].Id,
-                                ChannelSubscribers = searchChannelResponse.Items[0].Statistics.SubscriberCount.ToString(),
-                                ChannelVideos = searchChannelResponse.Items[0].Statistics.VideoCount.ToString(),
-                                ChannelViews = searchChannelResponse.Items[0].Statistics.ViewCount.ToString(),
-                                ChannelCreatedDate = searchChannelResponse.Items[0].Snippet.PublishedAt.ToString(),
-                                Email = email,
-                                Links = links
-                            };
-                            
-                            writer.Write(yTDataModel.BuildDataRow(yTDataModel));
-                            writer.Flush();
-                            fileStream.Flush();
-                            
-                            Console.WriteLine(rowCounter + "\t" + searchResult.Snippet.Title);
-                            rowCounter++;
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                            rowCounter++;
-                            continue;
-                        }  
-                    }
-                    pageCounter++;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    pageCounter++;
-                    continue;
-                }  
+                writer.WriteLine(item);
+                writer.Flush();
+                fileStream.Flush();
             }
+
             writer.Close();
             fileStream.Close();
-            Console.ReadLine();
+
         }
 
-        public static string GetAllCommentsAndReplies(CommentThreadListResponse searchCommentsResponse, CommentThreadsResource.ListRequest searchCommentsRequest)
-        {
-            string commentAndReplyText = null;
-            //while (searchCommentsResponse.NextPageToken.Length > 0)
-            {
-
-                foreach (var item in searchCommentsResponse.Items)
-                {
-
-                    string allComments = item.Snippet.TopLevelComment.Snippet.TextDisplay;
-                    string allReplies = null;
-                    if (item.Replies != null)
-                    {
-                        foreach (var reply in item.Replies.Comments)
-                        {
-                            allReplies += reply.Snippet.TextDisplay;
-                        }
-                    }
-                    commentAndReplyText += allComments + " " + allReplies;
-
-                    //searchCommentsRequest.PageToken = searchCommentsResponse.NextPageToken;
-                    //searchCommentsResponse = await searchCommentsRequest.ExecuteAsync();
-                }
-            }
-            return commentAndReplyText;
-        }
 
     }
 }
---
+++ YTSearchQueryAPI/YTDataModel.cs
@@ -0,0 +1,47 @@
+﻿namespace YTSearchQueryApiLib
+{
+    class YTDataModel
+    {
+        public int SNo { get; set; }
+        //Video
+        public string VideoUrl { get; set; }
+        public string VideoTitle { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoViews { get; set; }
+        public string VideoLikes { get; set; }
+        public string VideoDislikes { get; set; }
+        public string VideoPublishedDate { get; set; }
+
+        //Channel
+        public string ChannelName { get; set; }
+        public string ChannelDescription { get; set; }
+        public string ChannelUrl { get; set; }
+        public string ChannelSubscribers { get; set; }
+        public string ChannelVideos { get; set; }
+        public string ChannelViews { get; set; }
+        public string ChannelCreatedDate { get; set; }
+        public string Email { get; set; }
+        public string Links { get; set; }
+
+        public string BuildDataRow(YTDataModel yTDataModel)
+        {
+            string data = yTDataModel.SNo+
+                ","+VideoUrl+
+                ","+VideoTitle+
+                ","+VideoViews+
+                ","+VideoLikes+
+                ","+VideoDislikes+
+                ","+VideoPublishedDate+
+                ","+ChannelName+
+                ","+ChannelUrl+
+                ","+ChannelSubscribers+
+                ","+ChannelVideos+
+                ","+ChannelViews+
+                ","+ChannelCreatedDate+
+                "," + "\"" + Email.Trim() + "\"" +
+                "," + "\"" + Links.Trim() + "\"" +
+                "\n";
+            return data;
+        }
+    }
+}
